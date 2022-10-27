17998440c479e92072d4f415f0ba847676da2a06
Minh Pham <hpham76@my.centennialcollege.ca>
2022-10-27 14:55:20 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home', });
+});
+
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home', });
+});
+/* GET About page. */
+
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About', });
+});
+/* GET Project page. */
+
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Project', });
+});
+/* GET Service page. */
+
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Service', });
+});
+/* GET contact page. */
+
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact', });
+});
+
+
+module.exports = router;
