eacfbf12bbf21a8aef7dfa8ffe50f36362be08a6
Minh Pham <hpham76@my.centennialcollege.ca>
2022-11-03 22:15:54 -0400

    assignment2

--- routes/index.js
+++ routes/index.js
@@ -33,6 +33,20 @@ router.get('/services', function(req, res, next) {
 router.get('/contact', function(req, res, next) {
   res.render('contactpage.ejs', { title: 'Contact', });
 });
+/* GET Router for displaying the login page*/
+router.get('/login', indexController.displayLoginPage);
+/* POST Router for processing the Login Page*/
+router.post('/login', indexController.processLoginPage);
 
+
+/* GET Router for displaying register Page*/
+router.get('/register', indexController.displayRegisterPage);
+
+/* POST Router for processing the register Page*/
+router.post('/register', indexController.processRegisterPage);
+module.exports = router;
+
+
+router.get('/logout', indexController.performLogout);
 
 module.exports = router;
